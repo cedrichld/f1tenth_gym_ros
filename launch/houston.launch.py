697e67ea5e6cc94345cc3326f03965f27bc16885
Cedric Hollande <cedrich@seas.upenn.edu>
2026-05-12 12:29:04 -0400

    Add houston launch for opp testing

--- launch/houston.launch.py
+++ launch/houston.launch.py
@@ -233,7 +233,7 @@ def generate_launch_description():
     ld.add_action(
         DeclareLaunchArgument(
             'open_foxglove',
-            default_value='true',
+            default_value='false',
             description='Whether to open Foxglove in a browser.',
         )
     )
